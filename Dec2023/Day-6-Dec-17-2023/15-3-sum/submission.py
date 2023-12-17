1e61deea8bb61c7d9130cddbeba6ce3f88aefccf
Aditya Pavan Kumar <56818555+Adityapavankumar@users.noreply.github.com>
2023-12-17 06:43:00 +0530

    Create submission.py for 15

---
+++ Dec2023/Day-6-Dec-17-2023/15-3-sum/submission.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if len(nums) < 3:
+            return []
+        ans = []
+        nums.sort()
+        for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            left = i+1
+            right = len(nums) - 1
+            while left < right:
+                summ = nums[i] + nums[left] + nums[right]
+                if summ == 0:
+                    ans.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    while left < right and nums[left] == nums[left-1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right+1]:
+                        right -= 1
+                elif summ < 0:
+                    left += 1
+                else:
+                    right -= 1
+        return ans
