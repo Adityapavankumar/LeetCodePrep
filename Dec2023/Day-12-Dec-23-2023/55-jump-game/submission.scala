b01bd0bfd0f5cd417a9c4e633607fa0d606f3f25
Aditya Pavan Kumar <56818555+Adityapavankumar@users.noreply.github.com>
2023-12-23 05:09:07 +0530

    Create submission.scala for 55

---
+++ Dec2023/Day-12-Dec-23-2023/55-jump-game/submission.scala
@@ -0,0 +1,19 @@
+object Solution {
+    def canJump(nums: Array[Int]): Boolean = {
+        // iterate though all the indices before last index and calculate index + value at that index for each index
+        // if any of the value >= final index value, return true 
+        // if loop ends without such, return false
+        var maxReach = 0
+        val n = nums.length
+        for(i <- 0 to (n-1)){
+            if(i>maxReach){
+                return(false)
+            }
+            maxReach = Math.max(maxReach, i + nums(i))
+            if(maxReach>= (n-1)){
+                return(true)
+            }
+        }
+        return(false)
+    }
+}
