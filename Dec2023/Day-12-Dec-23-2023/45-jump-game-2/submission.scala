fe8b31d24f5d45f6be093ec94495aaa30ea831c7
Aditya Pavan Kumar <56818555+Adityapavankumar@users.noreply.github.com>
2023-12-23 05:47:20 +0530

    Create submission.scala for 45

---
+++ Dec2023/Day-12-Dec-23-2023/45-jump-game-2/submission.scala
@@ -0,0 +1,30 @@
+bject Solution {
+    def jump(nums: Array[Int]): Int = {
+        // iterate through indices and calculate maxReach at each place
+        // return the index where maxReach >= n -1
+        val n = nums.length
+        var maxReach = 0
+        var currentReach = 0
+        var jumpCount = 0
+        // handle one element array - if single element - no jumps needed
+        if(n==1){
+            return(0)
+        }
+        for(i <- 0 to (n-1)){
+            if(i>maxReach){
+                return(0)
+            }
+            maxReach = Math.max(maxReach, i + nums(i))
+            if(i==currentReach){
+                jumpCount += 1
+                currentReach = maxReach
+                if(maxReach>=(n-1)){
+                    return(jumpCount)
+                }
+            }
+        }
+        return(jumpCount)
+        
+        return(0)
+    }
+}
